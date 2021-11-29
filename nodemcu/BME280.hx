793ce152afd220442edd781c9746c58c05c61a59
mdrokz <33598169+mdrokz@users.noreply.github.com>
2021-11-29 14:04:48 +0530

    Completed BME280 module

--- nodemcu/BME280.hx
+++ nodemcu/BME280.hx
@@ -1,7 +1,30 @@
 package nodemcu;
 
+import haxe.extern.EitherType;
+
+@:multiReturn
+extern class Reading {
+	public var T:Float;
+	public var P:Float;
+	public var H:Float;
+	public var QNH:Float;
+}
+
 @:native("bme280")
 @:luaRequire
+@:luaDotMethod
 extern class BME280 {
-   
+	public function setup(id:Int, ?address:Int, ?temp_oss:Int, ?press_oss:Int, ?humi_oss:Int, ?sensor_mode:Int, ?inactive_duration:Int, ?IIR_filter:Int,
+		?cold_start:Int):BMEObject;
+}
+
+extern typedef BMEObject = {
+	public function setup(id:Int, ?address:Int, ?temp_oss:Int, ?press_oss:Int, ?humi_oss:Int, ?sensor_mode:Int, ?inactive_duration:Int, ?IIR_filter:Int,
+		?cold_start:Int):BMEObject;
+
+	public function altitude(P:Float, QNH:Float):Float;
+	public function dewpoint(H:Float, T:Float):Float;
+	public function qfe2qnh(P:Float, altitude:Float):Float;
+	public function read(?altitude:Array<Float>):EitherType<Void, Reading>;
+	public function startReadout(delay:Int, callback:(?T:Float, ?P:Float, ?H:Float, ?QNH:Float) -> Void):Void;
 }
