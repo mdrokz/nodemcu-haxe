41d6a23baa53e95c3d870cd6f2202aadf7d488fd
mdrokz <33598169+mdrokz@users.noreply.github.com>
2021-11-29 14:40:39 +0530

    completed DS18B20 module

---
+++ nodemcu/DS18B20.hx
@@ -0,0 +1,15 @@
+package nodemcu;
+
+import lua.Table;
+
+@:native("ds18b20")
+@:luaRequire
+@:luaDotMethod
+extern class DS18B20 {
+	public var C:String;
+	public var F:String;
+	public var temp:Table<Int, String>;
+	public var sens:Table<Int, Int>;
+	public function enable_debug():Void;
+	public function read_temp(callback:(temp:Table<Int, String>, pin:Int, ?unit:String, ?force_search:Bool, ?save_search:Bool) -> Void):Void;
+}
