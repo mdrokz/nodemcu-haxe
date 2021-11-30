08f93e75368e930b2d9752b2932758f557e74f51
mdrokz <33598169+mdrokz@users.noreply.github.com>
2021-11-30 15:19:08 +0530

    added reference link for YeeLink

--- nodemcu/YeeLink.hx
+++ nodemcu/YeeLink.hx
@@ -5,6 +5,7 @@ import haxe.extern.EitherType;
 @:native("yeelink")
 @:luaDotMethod
 @:luaRequire
+// for reference - https://nodemcu.readthedocs.io/en/release/lua-modules/yeelink/
 extern class YeeLink {
 	public function init(device:Int, sensor:Int, apiKey:String):EitherType<String, Bool>;
 	public function getDNS():EitherType<String, Void>;
