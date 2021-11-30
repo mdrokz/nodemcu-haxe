6f9c1683b7498068f5e118c0574fffe33dd81354
mdrokz <33598169+mdrokz@users.noreply.github.com>
2021-11-30 15:12:35 +0530

    added reference link for FTP

--- nodemcu/FTP.hx
+++ nodemcu/FTP.hx
@@ -2,6 +2,7 @@ package nodemcu;
 
 @:native("ftpserver")
 @:luaRequire
+// for reference - https://nodemcu.readthedocs.io/en/release/lua-modules/ftpserver/
 extern class FTP {
 	public function createServer(user: String,pass: String,?dbgFlag: Bool): Void;
     public function open(user: String,pass: String,ssid: String,?dbgFlag: Bool): Void;
