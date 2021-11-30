5576567bb2738ebc0539f692fcbabfd445946117
mdrokz <33598169+mdrokz@users.noreply.github.com>
2021-11-30 15:18:59 +0530

    added reference link for Telnet

--- nodemcu/Telnet.hx
+++ nodemcu/Telnet.hx
@@ -2,6 +2,7 @@ package nodemcu;
 
 @:native("telnet")
 @:luaRequire
+// for reference - https://nodemcu.readthedocs.io/en/release/lua-modules/telnet/
 extern class Telnet {
 	public function open(ssid:String, password:String, ?port:Int):Void;
 	public function close():Void;
