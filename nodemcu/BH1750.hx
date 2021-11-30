f677bbdf9a1dfad049c4c8c08fe4aaac2be8caf5
mdrokz <33598169+mdrokz@users.noreply.github.com>
2021-11-30 15:18:01 +0530

    added reference link for BH1750

--- nodemcu/BH1750.hx
+++ nodemcu/BH1750.hx
@@ -3,6 +3,7 @@ package nodemcu;
 @:native("bh1750")
 @:luaRequire
 @:luaDotMethod
+// for reference - https://nodemcu.readthedocs.io/en/release/lua-modules/bh1750/
 extern class BH1750 {
     public function init(sda: Int,scl: Int): Void;
     public function read(): Int;
