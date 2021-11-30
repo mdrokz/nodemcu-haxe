d72b07ad8b82cb252fc6012f2ab3d6cdf2e69f83
mdrokz <33598169+mdrokz@users.noreply.github.com>
2021-11-30 15:09:36 +0530

    added reference link for HDC1000

--- nodemcu/HDC1000.hx
+++ nodemcu/HDC1000.hx
@@ -3,6 +3,7 @@ package nodemcu;
 @:native("HDC1000")
 @:luaDotMethod
 @:luaRequire
+// for reference - https://nodemcu.readthedocs.io/en/release/lua-modules/hdc1000/
 extern class HDC1000 {
 	public function setup(drdyn:Int):Void;
 	public function config(address:Int, resolution:Int, heater:Int):Void;
