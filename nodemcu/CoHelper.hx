60b3c8d595513209e739b7c3987bbb2b815d7138
mdrokz <33598169+mdrokz@users.noreply.github.com>
2021-11-30 15:15:49 +0530

    added reference link for CoHelper

--- nodemcu/CoHelper.hx
+++ nodemcu/CoHelper.hx
@@ -3,6 +3,7 @@ package nodemcu;
 @:native("cohelper")
 @:luaRequire
 @:luaDotMethod
+// for reference - https://nodemcu.readthedocs.io/en/release/lua-modules/cohelper/
 extern class CoHelper {
 	public function exec<K, T>(func:(taskYield:(v:T) -> Void, list:Array<K>) -> Void, ...rest:K):T;
 }
