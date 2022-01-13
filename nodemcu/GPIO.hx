52e82fb71c8361083d179a1533a296aa83b3f31a
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-01-13 17:53:20 +0530

    update GPIO.hx with base methods

--- nodemcu/GPIO.hx
+++ nodemcu/GPIO.hx
@@ -4,12 +4,18 @@ package nodemcu;
 @:luaDotMethod
 // for reference - https://nodemcu.readthedocs.io/en/release/modules/gpio/#gpiomode
 extern class GPIO {
-	static public var OUTPUT:Int;
-	static public var INPUT:Int;
-	static public var OPENDRAIN:Int;
-	static public var INT:Int;
-	static public var PULLUP:Int;
-	static public var FLOAT:Int;
+	extern public static final OUTPUT:Int;
+	extern public static final INPUT:Int;
+	extern public static final OPENDRAIN:Int;
+	extern public static final INT:Int;
+	extern public static final PULLUP:Int;
+	extern public static final FLOAT:Int;
+	extern public static final HIGH:Int;
+	extern public static final LOW:Int;
 
 	public static function mode(pin:Int, mode:Int, ?pullup:Int):Void;
+
+	public static function read(pin:Int):Bool;
+
+	public static function write(pin:Int, level:Int): Void;
 }
