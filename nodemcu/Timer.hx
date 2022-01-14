9b786bed4acf50d551bad812e9cd1e2187d02ed0
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-01-14 17:40:41 +0530

    update Timer methods

--- nodemcu/Timer.hx
+++ nodemcu/Timer.hx
@@ -2,6 +2,7 @@ package nodemcu;
 
 @:native("tmr")
 @:luaDotMethod
-// for reference - https://nodemcu.readthedocs.io/en/release/modules/gpio/#gpiomode
+// for reference - https://nodemcu.readthedocs.io/en/release/modules/tmr/
 extern class Timer {
+    public static function delay(us: Float): Void;
 }
